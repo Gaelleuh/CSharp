5f87340f08ec6a410a02ca7cd6dd52dc70322611
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-02 14:56:17 +0100

    Add files via upload
    ajout de l'exo 1

---
+++ Exo01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Console.WriteLine("*** Ma liste de todo ***");
+//Console.WriteLine("Aujourd'hui je dois faire :");
+//Console.WriteLine("         -Apprendre le C#");
+//Console.WriteLine("         -Apprendre à utiliser Visual Studio");
+//Console.WriteLine("         -Comprendre l'affichage \"Console\"");
+//Console.WriteLine("         -Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger");
+//Console.WriteLine("         -Apprécier les fonctionnalités du C#");
+//Console.WriteLine("\n\n\n");
+
+Console.WriteLine("*** Ma liste de todo ***\n" +
+    "Aujourd'hui je dois faire :\n" +
+    "         -Apprendre le C#\n" +
+    "         -Apprendre à utiliser Visual Studio\n" +
+    "         -Comprendre l'affichage \"Console\"\n" +
+    "         -Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger\n" +
+    "         -Apprécier les fonctionnalités du C#\n");
