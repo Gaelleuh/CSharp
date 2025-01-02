84ae341f1a3c7801d1d08cd1676a27758b87e0a2
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-02 15:43:58 +0100

    Add files via upload
    Ajout de la deuxième partie de l'Exo05

---
+++ Exo05/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Veuillez saisir un nombre 1 : ");
+double nb1 = Convert.ToDouble(Console.ReadLine().Replace('.',','));
+
+Console.Write("Veuillez saisir un nombre 2 : ");
+double nb2 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.WriteLine("La somme de ces deux nombres est : " + (nb1 + nb2));
+
