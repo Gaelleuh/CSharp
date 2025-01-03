e6984fef342e729113364d363ed63b0361219f21
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-03 11:55:48 +0100

    Add files via upload

---
+++ Exo08/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Entrez le prix HT de l'objet (en Euros) : ");
+double prixObjet = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.Write("Entrez le taux de TVA (en %) : ");
+double tauxTva = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+double montantTva = (prixObjet* tauxTva)/100;
+double montantTTC = prixObjet + montantTva ;
+
+Console.WriteLine("Le montant de la TVA est de " + montantTva+ " euros");
+Console.WriteLine("Le prix TTC de l'objet est de " + montantTTC + " euros");
---
+++ Exo09/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("*** Calcul des intérêts ***\n");
+
+Console.Write("Entrez le Capital de départ (en Euros) : ");
+double capitalDepart = double.Parse(Console.ReadLine());
+
+Console.Write("Entrez le taux d'intérêts (en %) : ");
+double tauxInteret = double.Parse(Console.ReadLine());
+
+Console.Write("Entrez la durée de l'épargne (en années) : ");
+int epargneAnnees = int.Parse(Console.ReadLine());
+
+double montantInteret = Math.Round(capitalDepart*Math.Pow((1+tauxInteret/100),epargneAnnees),2);
+
+double capitalFinal = Math.Round((capitalDepart+montantInteret),2);
+
+Console.WriteLine($"Le montant des intérêts sera de {montantInteret} après {epargneAnnees} ans");
+Console.WriteLine($"Le capital final sera de {capitalFinal}");
