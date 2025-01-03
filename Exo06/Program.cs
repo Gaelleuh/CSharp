3285199ac2b57e6f1bf11d1736ecd2d37e28bc67
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-03 10:32:21 +0100

    Add files via upload
    Ajout de l'exo 6 et 7

---
+++ Exo06/Program.cs
@@ -0,0 +1,23 @@
+﻿//--------------------------CARRÉ----------------------------------//
+
+Console.WriteLine("*** Calcul du périmètre et de l'aire d'un carré ***\n");
+Console.Write("Entrez la longueur d'un côté du carré (en cm) : ");
+
+double sidesquare = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.WriteLine("Le périmètre du carré est de: " + (sidesquare*4) + " cm");
+Console.WriteLine("L'aire du carré est de : " + (sidesquare * sidesquare) + " cm²");
+Console.Write("\n");
+//------------------------RECTANGLE--------------------------------//
+
+Console.WriteLine("*** Calcul du périmètre et de l'aire d'un rectangle ***\n");
+Console.Write("Entrez la longueur d'un côté du rectangle (en cm) : ");
+
+double siderect1 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.Write("Entrez la longueur d'un autre côté du rectangle (en cm) : ");
+
+double siderect2 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.WriteLine("Le périmètre du rectangle est de : " + ((siderect1 * 2)+(siderect2*2))+" cm");
+Console.WriteLine("L'aire du rectangle est de : " + (siderect1*siderect2)+" cm²");
---
+++ Exo07/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("*** Calcul de la longueur de l'hypothénuse ***\n");
+
+Console.Write("Entrez la longueur du premier côté (en cm) : ");
+double side1 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+Console.Write("Entrez la longueur du deuxième côté (en cm) : ");
+double side2 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+double side1_2 = Math.Round(Math.Pow(side1, 2), 2);
+
+double side2_2 = Math.Round(Math.Pow(side2, 2), 2);
+
+double hypo = Math.Round(Math.Sqrt(side1_2 + side2_2), 2);
+
+Console.WriteLine("La longueur de l'hypothénuse est de "+ hypo + " cm²");
+Console.Write("\n");
