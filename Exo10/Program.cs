0216626cfadabc894a809589c6b99b06ab383429
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-03 15:48:17 +0100

    Add files via upload
    ajout exo 10

---
+++ Exo10/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("*** La lettre est-elle une voyelle ***\n");
+
+Console.Write("Entrez une lettre de l'alphabet : ");
+
+string lettre = Console.ReadLine().ToUpper();
+
+if (lettre == "A" || lettre == "E" || lettre == "I" || lettre == "O" || lettre == "U" || lettre == "Y")
+     
+
+    Console.WriteLine("Cette lettre est une voyelle \n");
+
+else 
+    Console.WriteLine("Cette lettre est une consonne \n");
