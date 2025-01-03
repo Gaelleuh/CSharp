dfa4ea4fadb1bfee8cc7ed5d64348d623027fbe1
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-03 15:49:39 +0100

    Add files via upload
    ajout exo13

---
+++ Exo13/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("*** Quelle est la nature du triangle ABC ? ***\n");
+
+Console.Write("Entrez la longueur du segment AB : ");
+double AB = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entrez la longueur du segment BC : ");
+double BC = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entrez la longueur du segment CA : ");
+double CA = Convert.ToDouble(Console.ReadLine());
+
+if (AB == BC)
+{
+    if (BC == CA)
+    {
+        Console.WriteLine("Le triangle est équilatéral\n");
+    }
+
+    else
+    {
+        Console.WriteLine("Le triangle est isocèle en B\n");
+        }
+}
+
+else if (AB == CA)
+{
+    Console.WriteLine("Le triangle est isocèle en A\n");
+}
+
+else if (BC == CA)
+{
+    Console.WriteLine("Le triangle est isocèle en C\n");
+}
+
+else
+{
+    Console.WriteLine("Le triangle n'est isocèle ni en A, ni en B, ni en C");
+}
