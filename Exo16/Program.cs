98a006cb587fa66af9b30fcc569d41324add3931
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-06 11:10:12 +0100

    Add files via upload
    ajout exo 16 et 17

---
+++ Exo16/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("--- Quelle boisson souhaitez-vous ? ---\n");
+
+Console.WriteLine("Liste des boissons disponibles :\n" +
+    "1) Eau Plate\n" +
+    "2) Eau Gazeuse\n" +
+    "3) Coca-Cola\n" +
+    "4) Fanta\n" +
+    "5) Sprite\n" +
+    "6) Orangina\n" +
+    "7) 7Up\n");
+
+Console.Write("Faites votre choix en tapant le chiffre correspondant(1 à 7)\n");
+Console.Write("\n");
+int choix = Convert.ToInt32(Console.ReadLine());
+
+switch (choix)
+{
+    case 1:
+        Console.WriteLine("Votre Eau Plate est servie !\n");
+        break;
+    case 2:
+        Console.WriteLine("Votre Eau Gazeuse est servie !\n");
+        break;
+    case 3:
+        Console.WriteLine("Votre Coca-Cola est servi !\n");
+        break;
+    case 4:
+        Console.WriteLine("Votre Fanta est servi !\n");
+        break;
+    case 5:
+        Console.WriteLine("Votre Sprite est servi !\n");
+        break;
+    case 6:
+        Console.WriteLine("Votre Orangina est servi !\n");
+        break;
+    case 7:
+        Console.WriteLine("Votre 7Up est servi !\n");
+        break;
+    default:
+        Console.WriteLine("Votre choix n'est pas valide\n");
+        break;
+}
+
+
+
+
+
+
---
+++ Exo17/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("--- Dans quelle catégorie mon enfant est-il ... ? ---\n");
+
+Console.Write("Entrez l'âge de votre enfant :\n");
+Console.Write("\n");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.Write("\n");
+switch (age)
+{
+    case < 3:
+        Console.WriteLine("Votre enfant est trop jeune !");
+        break;
+    case >= 3 and <=6:
+        Console.WriteLine("Votre enfant est dans la catégorie \"Baby\" !");
+        break;
+    case >= 7 and <= 8:
+        Console.WriteLine("Votre enfant est dans la catégorie \"Poussin\" !");
+        break;
+    case >= 9 and <= 10:
+        Console.WriteLine("Votre enfant est dans la catégorie \"Pupille\" !");
+        break;
+    case >= 11 and <= 12:
+        Console.WriteLine("Votre enfant est dans la catégorie \"Minime\" !");
+        break;
+    case >= 13 and <= 17:
+        Console.WriteLine("Votre enfant est dans la catégorie \"Cadet\" !");
+        break;
+    default:
+        Console.WriteLine("Votre enfant est trop vieux !");
+        break;
+}
