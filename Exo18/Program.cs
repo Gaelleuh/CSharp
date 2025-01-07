8396daeac97d00fbc3a3c1f3c42ed960477967b8
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-07 09:23:01 +0100

    Add files via upload
    ajout exo 18 et 19

---
+++ Exo18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Je compte jusqu'à 10 ---\n");
+
+Console.WriteLine("Je commence à compter...\n");
+
+int compteur = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    compteur++;
+    Console.WriteLine($"\t{compteur}");  // \t = tabulation
+}
+Console.WriteLine("\n");
+Console.WriteLine("Super, je sais compter jusqu'à 10 !");
---
+++ Exo19/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("--- Menus et sous-menus ---\n");
+
+Console.WriteLine("Table des matières\n");
+
+
+for (int chapitre=1; chapitre<=3;chapitre++)
+{
+    Console.WriteLine($"Chapitre {chapitre}");
+    
+    for (int partie=1; partie<=3;partie++)
+    {
+        Console.WriteLine($"\t\t Partie {chapitre}.{partie}");
+    }
+}
