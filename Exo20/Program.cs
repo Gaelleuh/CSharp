ce29e75d121b0af2960837d9f0992fb85f66be28
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-07 09:23:51 +0100

    Add files via upload
    ajout exo 20 et 21

---
+++ Exo20/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- Les tables de multiplication ---\n");
+
+for (int chiffre = 1; chiffre <= 10; chiffre++)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine($"Table de {chiffre}\n");
+    for (int multiplicateur=1; multiplicateur <= 10; multiplicateur++)
+    {
+        Console.WriteLine($"    {chiffre} x {multiplicateur} = {chiffre*multiplicateur}");
+    }
+    Console.WriteLine("\n");
+}
---
+++ Exo21/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("--- Accroissement de la population ---\n");
+
+double nbrAnnees = 0;
+double a2015 = 96809;
+double a2xxx = 120000;
+double taux = 0.0089;
+double anneeDepart = 2015;
+
+while (a2015 <= a2xxx)
+{
+    a2015 = (a2015 * (1 + taux));
+    nbrAnnees = nbrAnnees + 1;
+}
+double addition = anneeDepart + nbrAnnees;
+
+Console.WriteLine(@$"il faudra {nbrAnnees} ans pour que la ville de Tourcoing atteigne 120000 habitants.
+Nous serons donc en {addition}.
+Il y aura {Math.Round(a2015,0)} habitants en {addition}.");
