dabcadfa2c6dfbca20e8c8a84348ccbbe7863dbf
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-07 15:55:06 +0100

    Add files via upload
    ajout exo 23 et 24

---
+++ Exo23/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("*_* Le triangle formé d'étoiles *_*\n");
+
+//Console.Write("Saisir la hauteur du triangle(entier) : ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//string message = "";
+
+//for (int i = 0; i <= number; i=i+1)
+//{
+//    message += "*";
+//    Console.WriteLine(message);
+//}
+
+
+//------CORRECTION----------//
+
+Console.Write("Saisir la hauteur du triangle(entier) : ");
+int hauteur = Convert.ToInt32(Console.ReadLine());
+
+//-----------Dessiner le triangle------------//
+
+for (int ligne= 1;ligne <= hauteur;ligne++)
+{
+    //-----Espaces avant les astérisques pour centrer le triangle------//
+    for (int espace=1;espace<=hauteur -ligne;espace++)
+    {
+        Console.WriteLine(" ");
+    }
+    for (int etoile = 1;etoile <=2*ligne-1; etoile++)
+    {
+        //-----Astérisques à afficher------------//
+        Console.Write("*");
+    }
+    //--------Nouvelle ligne pour le prochain niveau du triangle--------//
+    Console.WriteLine();
+}
---
+++ Exo24/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("*_* Le Sapin de Noël *_*\n");
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; //permet l'utilisation des emotes
+
+Console.Write("Saisir la hauteur du triangle(entier) : ");
+int hauteur = Convert.ToInt32(Console.ReadLine());
+
+List<ConsoleColor> ballColors = new List<ConsoleColor>()
+{   ConsoleColor.Red,
+    ConsoleColor.Yellow,
+    ConsoleColor.Cyan,
+    ConsoleColor.Magenta 
+};
+
+Random random = new Random();
+
+while (true)
+{
+    for (int ligne = 1; ligne <= hauteur; ligne++)
+    {
+        for (int espace = 1; espace <= hauteur - ligne; espace++)
+        {
+            Console.Write(" ");
+        }
+        if (ligne == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("😻");
+            continue;
+        }
+        for (int etoile = 1; etoile <= 2 * ligne - 1; etoile++)
+        {
+            string symb;
+            if (random.NextDouble() > 0.40)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                symb="*";
+            }
+            else
+            {
+                Console.ForegroundColor = ballColors[random.Next(ballColors.Count)];
+                symb = "¤";
+            }
+            Console.Write(symb);
+        }
+        Console.WriteLine();
+    }
+    Thread.Sleep(1000);
+    Console.Clear();
+}
