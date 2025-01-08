fc788dddee23be145a31d7f83dbcc31898bc5353
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-08 16:00:50 +0100

    Add files via upload
    ajout exo 30 et 31

---
+++ Exo30/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("*_* Est pair...? Est impair...? *_*\n");
+
+int numero = 0;
+Random remplissageAuto = new Random();
+
+Console.Write("Combien de nombre contiendra le tableau ? : ");
+int nbrInTab = Convert.ToInt32(Console.ReadLine());
+int[] valeurs = new int[nbrInTab];
+
+Console.WriteLine("Affectation automatique des valeurs du tableau :\n");
+
+for (numero = 0; numero < valeurs.Length; numero++)
+{
+    int val = remplissageAuto.Next(1, 101);
+    valeurs[numero] = val;
+
+    if (val % 2 == 0)
+    {
+        Console.WriteLine($"Le nombre {val} est Pair");
+    }
+    else
+        Console.WriteLine($"Le nombre {val} est Impair");
+}
---
+++ Exo31/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.WriteLine("*_* GESTION DES CONTACTS *_*\n");
+
+Console.Write("Merci de saisir le nombre de contacts : ");
+int nbrContacts = int.Parse(Console.ReadLine());
+Console.WriteLine("\n");
+
+string newContact;
+string[] listeContacts = new string[nbrContacts];
+int choice;
+Console.Clear();
+
+do
+{
+    Console.WriteLine(@"==== MA LISTE DE CONTACTS ====
+1----Saisir des contacts
+2----Afficher mes contacts
+0----Quitter");
+
+    Console.Write("Faites votre choix : ");
+    choice = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+
+    switch (choice)
+    {
+        case 1:
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("==== SAISIR LES CONTACTS ====");
+            Console.ResetColor();
+            for (int i = 0; i < nbrContacts; i++)
+            {
+                Console.Write($"Nom et prénom du contact n°{i+1} : ");
+                newContact = Console.ReadLine();
+                listeContacts[i] = newContact;
+            }
+            Console.Clear();
+            break;
+
+        case 2:
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("==== AFFICHAGE DES CONTACTS ====");
+            Console.ResetColor();
+            for (int i = 0; i < listeContacts.Length; i++)
+            {
+                Console.WriteLine($"- Contact n°{i + 1} : {listeContacts[i]} \n");
+            }
+            Thread.Sleep(3000);
+            Console.Clear() ;
+            break;
+        
+        default:
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Erreur de saisie, recommencer !");
+                Console.ResetColor();
+            }
+            Console.Clear();
+            break;
+    }
+} while (choice!=0);
+Environment.Exit(0);
+Console.Clear();
+
+///---------------CORRECTION---------------//
+
+
+
