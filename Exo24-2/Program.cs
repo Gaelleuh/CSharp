59d9c5e56276799048d44a0d7d9f5bb1d01d4e01
Gaëlle QUEFFELEC <138670881+Gaelleuh@users.noreply.github.com>
2025-01-08 15:59:06 +0100

    Add files via upload
    ajout exo 24-2 , 25 et 26

---
+++ Exo24-2/Program.cs
@@ -0,0 +1,105 @@
+﻿Console.WriteLine("*_* Question à choix multiple *_*\n");
+
+Console.Write(@"Quelle est l'instruction qui permet de sortir d'une boucle en C# ?
+        a) quit
+        b) continue
+        c) break
+        d) exit
+
+");
+
+
+string choix;
+string choixOuiNon;
+
+Console.Write("\n");
+
+do
+    {
+    Console.Write("Entrez votre réponse : ");
+    choix = Console.ReadLine().ToUpper();
+    if (choix == "A" || choix == "B" || choix == "D")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Réponse Incorrecte\n");
+        Console.ResetColor();
+        Console.Write("Un nouvel essai ? (Oui/Non): \n");
+        Console.Write("\n");
+
+        choixOuiNon = Console.ReadLine().ToUpper();
+
+
+        if (choixOuiNon == "OUI")
+        {
+            Console.Write("Entrez votre réponse : ");
+            choix = Console.ReadLine().ToUpper();
+        }
+
+        if (choixOuiNon == "NON")
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Tant pis !\n");
+            Console.ResetColor();
+            break;
+        }
+
+        if (choixOuiNon != "OUI" || choixOuiNon != "NON")
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Merci de répondre oui ou non\n");
+            Console.ResetColor();
+            choixOuiNon = Console.ReadLine().ToUpper();
+            Console.Write("\n");
+        }
+
+    }
+    else if (choix == "C")
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Bravo ! C'est la bonne réponse !");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Réponse Incorrecte\n");
+        Console.ResetColor();
+        Console.Write("Un nouvel essai ? (Oui/Non): \n");
+        Console.Write("\n");
+
+        choixOuiNon = Console.ReadLine().ToUpper();
+
+
+        if (choixOuiNon == "OUI")
+        {
+            Console.Write("Entrez votre réponse : ");
+            choix = Console.ReadLine().ToUpper();
+        }
+
+        if (choixOuiNon == "NON")
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Tant pis !\n");
+            Console.ResetColor();
+            break;
+        }
+
+        if (choixOuiNon != "OUI" || choixOuiNon != "NON")
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Merci de répondre oui ou non\n");
+            Console.ResetColor();
+            choixOuiNon = Console.ReadLine().ToUpper();
+            Console.Write("\n");
+        }
+    }
+} while (choix != "C");
+
+
+
+
+
+
+
+
+
---
+++ Exo26/Program.cs
@@ -0,0 +1,66 @@
+﻿//Console.WriteLine("*_* Le Nombre Mystère *_*\n");
+
+//Random aleatoire = new Random();
+//int nbrMystere = aleatoire.Next(1, 51);
+////Console.WriteLine(nbrMystere); //écrit le nombre dans le console
+
+//int guess = 0, nbrEssais = 0;
+
+
+//while (guess != nbrMystere)
+//{
+//    Console.Write("Veuillez saisir un nombre entre 1 et 50 : ");
+
+//    guess = Convert.ToInt32(Console.ReadLine());
+
+//    while (!int.TryParse(Console.ReadLine(), out guess) || guess < 1 || guess > 50) // vérifie le type et si le nombre est bien entre 1 et 50
+//        {
+//        Console.WriteLine("Le nombre que vous avez saisi n'est pas entre 1 et 50");
+//    }
+//}  
+//nbrEssais++;
+
+//if (guess > nbrMystere)
+//{
+//    Console.WriteLine($"Vous avez saisi {guess}, Cherchez encore ! C'est moins !");
+//}
+
+//else if (guess < nbrMystere)
+//{
+//    Console.WriteLine($"Vous avez saisi {guess}, Cherchez encore ! C'est plus !");
+//}
+//    Console.WriteLine($"Bravo ! le nombre était {guess}, vous avez trouvé ! Trop Fort !");
+//    Console.WriteLine($"Vous avez trouvé en {nbrEssais} coups.");
+
+//----CORRECTION------//
+
+Random aleatoire = new Random();
+int nbMystere = aleatoire.Next(1, 51);
+int choix = 0, nbCoups = 0;
+
+while (choix != nbMystere)
+{
+    Console.Write("Veuillez saisir un nombre entre 1 et 50 : ");
+    while (!int.TryParse(Console.ReadLine(), out choix) || choix < 1 || choix > 50)
+    {
+        Console.WriteLine("Saisie invalide ! ");
+    }
+    nbCoups++;
+
+    if (choix > nbMystere)
+    {
+        Console.WriteLine("Le nombre mystère est plus petit !");
+    }
+    else if (choix < nbMystere)
+    {
+        Console.WriteLine("Le nombre mystère est plus grand !");
+    }
+}
+
+Console.WriteLine("Bravo !! vous avez trouvé le nombre mystère !");
+Console.WriteLine($"Vous avez trouvé en {nbCoups} coups");
+
+
+
+
+
